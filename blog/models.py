a3ac87e902271048a7253e43ecd278c88da3e9bd
pro <pro@prodeMacBook-Pro.local>
2021-04-16 13:41:09 +0600

    содание админки

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Post(models.Model):
+    title = models.CharField(verbose_name='Заголовок' ,max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='ссылка', unique=True)
+    author = models.CharField(verbose_name='Автор' ,max_length=100)
+    content = models.TextField(verbose_name='Контент',blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photo/%y/%m/%d', blank=True, verbose_name='Фото')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='Категории')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='Теги')
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
